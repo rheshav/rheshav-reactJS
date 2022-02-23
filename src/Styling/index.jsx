f0e30475071036b97621c0515d204ecbee5a7940
rheshav <rhesa.havilah@gmail.com>
2022-02-23 15:04:06 +0700

    styling

--- src/Styling/index.jsx
+++ src/Styling/index.jsx
@@ -3,12 +3,14 @@ import Plain from './Plain';
 import Sass from './Sass';
 import Title from './Inline';
 import Module from './Module';
+import Styled from './StyledComponents';
+import Bootstrap from './Bootstrap';
 
 export default class Styling extends React.Component {
   render() {
     return (
       <div>
-        <Module />
+        <Bootstrap />
       </div>
     );
   }
