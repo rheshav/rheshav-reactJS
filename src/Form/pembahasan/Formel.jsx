9beabe1dec2569d8d14b5fb510fb3a3b2a0ce7ed
rheshav <rhesa.havilah@gmail.com>
2022-04-19 15:42:09 +0700

    ketambahan dikit

---
+++ src/Form/pembahasan/Formel.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Formel extends React.Component {
+  render() {
+    <form>
+      <label>
+        Nama : <input type="text"></input>
+      </label>
+      <label>
+        Jurusan :
+        <select>
+          <option value="">Pilih Jurusan</option>
+          <option value="Teknik Informatika">Teknik Informatika</option>
+          <option value="Sistem Informasi">Sistem Informasi</option>
+          <option value="Design Komunikasi Visual">Design Komunikasi Visual</option>
+        </select>
+      </label>
+      <label>
+        Jenis Kelamin:
+        <input type="radio" value="laki-laki"></input>
+        <input type="radio" value="perempuan"></input>
+      </label>
+      <label>
+        Member: <input type="c" />
+      </label>
+    </form>;
+    return <div>a</div>;
+  }
+}
+
+export default Formel;
