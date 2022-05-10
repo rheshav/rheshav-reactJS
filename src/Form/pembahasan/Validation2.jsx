d4a7d1913fcadfb63a3266c2280f7986c9b051da
rheshav <rhesa.havilah@gmail.com>
2022-05-10 12:31:00 +0700

    last edit sign in

--- src/Form/pembahasan/Validation2.jsx
+++ src/Form/pembahasan/Validation2.jsx
@@ -73,12 +73,12 @@ export default class Validation2 extends React.Component {
     return (
       <div style={style}>
         {this.state.errors && <ShowErrors errors={this.state.errors}></ShowErrors>}
-        <h4>Sing-in</h4>
+        <h4>Sign-in</h4>
         <form onSubmit={this.handleSubmit}>
           <Input type="email" name="email" label="Email" onChange={(value) => this.setState({ email: value })} />
           <Input type="password" name="password" label="Password" onChange={(value) => this.setState({ password: value })} />
           <br />
-          <button type="submit">Sing-in</button>
+          <button type="submit">Sign-in</button>
         </form>
       </div>
     );
